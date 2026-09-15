f23e3179180d8f35f2d231c58efbb35b9ddb347a
andrey1890 <andrey1890@outlook.es>
2026-09-15 11:28:52 -0500

    Dejar solo index y estilos

--- hola mundo/script.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  const year = new Date().getFullYear();
-  const footer = document.querySelector('footer span:last-child');
-  if (footer) {
-    footer.textContent = `${footer.textContent} · ${year}`;
-  }
-})();
